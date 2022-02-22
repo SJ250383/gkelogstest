860f1471c345be3357ba28b8406a0d37e629abea
SJ250383 <100226611+SJ250383@users.noreply.github.com>
2022-02-22 23:54:42 +0530

    logs app

---
+++ GKELogsApp/Controllers/LogsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Controllers
+{
+    [Route("Logs")]
+    public class LogsController : Controller
+    {
+        
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("LogWork1")]
+        public IActionResult LogWork1()
+        {
+            return View("LogWork1");
+        }
+
+        [HttpPost]
+        [Route("LogWork2")]
+        public IActionResult LogWork2()
+        {
+            return View("LogWork2");
+        }
+
+       
+    }
+}
